f40f7396042bb062e5bb31a4b3063b6d40063e8b
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-07-01 23:42:49 +0700

    initialize express app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var routes = require('./routes')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(routes)
+
+module.exports = app;
