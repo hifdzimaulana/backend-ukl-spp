08eae4d6ad2860e2b63f647c240892409c4eff58
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-07-03 14:42:17 +0700

    create database migrations, models, & relationship

---
+++ models/petugas.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Petugas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Pembayaran, { as: 'pembayaran', foreignKey: 'idPetugas' })
+    }
+  }
+  Petugas.init({
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    namaPetugas: DataTypes.STRING,
+    level: DataTypes.ENUM('superadmin', 'admin', 'owner')
+  }, {
+    sequelize,
+    modelName: 'Petugas',
+    freezeTableName: true
+  });
+  return Petugas;
+};
---
+++ models/siswa.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Siswa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Kelas, { foreignKey: "idKelas", as: "kelas" });
+      this.hasMany(models.Pembayaran, {
+        foreignKey: "idSiswa",
+        as: "pembayaran",
+      });
+    }
+  }
+  Siswa.init(
+    {
+      nama: DataTypes.STRING,
+      idKelas: DataTypes.INTEGER,
+      alamat: DataTypes.TEXT,
+      telepon: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Siswa",
+      freezeTableName: true,
+    }
+  );
+  return Siswa;
+};
